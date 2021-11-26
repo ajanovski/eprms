67767a0026b9ba5cf8925fd1aaaf85410a41064a
Vangel V. Ajanovski <development-vva@ajanovski.info>
2021-11-26 17:56:14 +0100

    Migrate from Criteria API to Query

--- eprms-tap/src/main/java/info/ajanovski/eprms/tap/services/TranslationServiceImpl.java
+++ eprms-tap/src/main/java/info/ajanovski/eprms/tap/services/TranslationServiceImpl.java
@@ -1,5 +1,7 @@
 package info.ajanovski.eprms.tap.services;
 
+import java.util.List;
+
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -15,16 +17,23 @@ public class TranslationServiceImpl implements TranslationService {
 	@Inject
 	private GenericService genericService;
 
+	private Session getEntityManager() {
+		return session.getSession();
+	}
+
 	@Override
 	public String getTranslation(String className, String attributeCode, long originalObjectId, String locale) {
 		try {
-			Criteria crit = session.createCriteria(Translation.class)
-					.add(Restrictions.and(Restrictions.eq("className", className),
-							Restrictions.eq("attributeCode", attributeCode), Restrictions.eq("locale", locale),
-							Restrictions.eq("originalObjectId", originalObjectId)))
-					.setReadOnly(true);
-
-			return ((Translation) crit.uniqueResult()).getTranslatedText();
+			Translation t = (Translation) getEntityManager().createQuery("""
+					from Translation
+					where className=:className and
+					      attributeCode=:attributeCode and
+					      originalObjectId=:originalObjectId and
+					      locale=:locale
+					""").setParameter("className", className).setParameter("attributeCode", attributeCode)
+					.setParameter("originalObjectId", originalObjectId).setParameter("locale", locale)
+					.getSingleResult();
+			return t.getTranslatedText();
 		} catch (Exception e) {
 			return null;
 		}
