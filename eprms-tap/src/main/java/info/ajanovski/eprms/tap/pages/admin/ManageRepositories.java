c5463c66dc8aa2a8b7296c7d651d1ce22731225c
Vangel V. Ajanovski <development-vva@ajanovski.info>
2023-05-28 01:44:35 +0200

    repository editing and activation

--- eprms-tap/src/main/java/info/ajanovski/eprms/tap/pages/admin/ManageRepositories.java
+++ eprms-tap/src/main/java/info/ajanovski/eprms/tap/pages/admin/ManageRepositories.java
@@ -1,12 +1,15 @@
 package info.ajanovski.eprms.tap.pages.admin;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
+import info.ajanovski.eprms.model.entities.Database;
 import info.ajanovski.eprms.model.entities.Repository;
 import info.ajanovski.eprms.tap.annotations.AdministratorPage;
 import info.ajanovski.eprms.tap.annotations.InstructorPage;
@@ -23,17 +26,45 @@ public class ManageRepositories {
 	@Persist
 	private Boolean isOnlyShowNotCreated;
 
+	@Property
+	private Repository repository;
+
+	@Persist
+	@Property
+	private Repository editRepository;
+
 	public void onActivate() {
 		if (isOnlyShowNotCreated == null) {
 			isOnlyShowNotCreated = false;
 		}
 	}
 
-	@Property
-	private Repository repository;
-
 	public void onActionFromToggle() {
 		isOnlyShowNotCreated = !isOnlyShowNotCreated;
+	}
+
+	public void onActionFromEdit(Repository r) {
+		editRepository = r;
+	}
+
+	@CommitAfter
+	public void onActionFromDelete(Repository r) {
+		if (r.getDateCreated() == null) {
+			genericService.delete(r);
+		}
+	}
+
+	@CommitAfter
+	public void onActionFromActivateRepositories() {
+		for (Repository r : getRepositories()) {
+			r.setDateCreated(new Date());
+		}
+	}
+
+	@CommitAfter
+	public void onSuccessFromEditForm() {
+		genericService.saveOrUpdate(editRepository);
+		editRepository = null;
 	}
 
 	public List<Repository> getRepositories() {
@@ -44,4 +75,6 @@ public class ManageRepositories {
 			return ls;
 		}
 	}
+
+
 }
