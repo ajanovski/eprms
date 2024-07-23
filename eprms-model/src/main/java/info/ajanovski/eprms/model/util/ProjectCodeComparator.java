354ac4877eb8f5f8bfcc6e6b1f2084accfada2a5
Vangel V. Ajanovski <development-vva@ajanovski.info>
2024-07-23 20:17:17 +0200

    bump version to include project code comparator

---
+++ eprms-model/src/main/java/info/ajanovski/eprms/model/util/ProjectCodeComparator.java
@@ -0,0 +1,11 @@
+package info.ajanovski.eprms.model.util;
+
+import java.util.Comparator;
+
+import info.ajanovski.eprms.model.entities.Project;
+
+public class ProjectCodeComparator implements Comparator<Project> {
+	public int compare(Project p1, Project p2) {
+		return p1.getCode().compareTo(p2.getCode());
+	}
+}
