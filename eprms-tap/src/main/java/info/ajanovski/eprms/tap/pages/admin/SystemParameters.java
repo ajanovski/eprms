27b3c49e068058239e262efb7783f1a6d9368f3a
Vangel V. Ajanovski <development-vva@ajanovski.info>
2026-03-09 18:03:28 +0100

    page/SystemParameters eliminate @Persist where not needed

--- eprms-tap/src/main/java/info/ajanovski/eprms/tap/pages/admin/SystemParameters.java
+++ eprms-tap/src/main/java/info/ajanovski/eprms/tap/pages/admin/SystemParameters.java
@@ -22,11 +22,14 @@ package info.ajanovski.eprms.tap.pages.admin;
 
 import java.util.List;
 
-import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.EventContext;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
+import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.slf4j.Logger;
 
 import info.ajanovski.eprms.model.entities.SystemParameter;
 import info.ajanovski.eprms.tap.annotations.AdministratorPage;
@@ -44,23 +47,161 @@ public class SystemParameters {
 	@Inject
 	private GenericService genericService;
 
-	@Property
-	private SystemParameter systemParameter;
+	@Inject
+	Logger logger;
 
-	@Persist
+	@InjectComponent
+	private Form frmCreateSystemParameter;
+
+	@InjectComponent
+	private Form updateForm;
+
+	public enum Mode {
+		CREATE, UPDATE
+	}
+
+	// activation context
+
 	@Property
-	private SystemParameter editSystemParameter;
+	private Mode editorMode;
+
+	@Property
+	private Long editorSystemParamId;
+
+	// properties
+
+	@Property
+	private SystemParameter listedSystemParameter;
+
+	@Property
+	private SystemParameter editorSystemParam;
+
+	public boolean isModeCreate() {
+		return editorMode == Mode.CREATE;
+	}
+
+	public boolean isModeUpdate() {
+		return editorMode == Mode.UPDATE;
+	}
+
+	// The code
+
+	void onActivate(EventContext ec) {
+		if (ec.getCount() == 0) {
+			editorMode = null;
+			editorSystemParamId = null;
+		} else if (ec.getCount() == 1) {
+			editorMode = ec.get(Mode.class, 0);
+			editorSystemParamId = null;
+		} else {
+			editorMode = ec.get(Mode.class, 0);
+			editorSystemParamId = ec.get(Long.class, 1);
+		}
+	}
+
+	Object[] onPassivate() {
+		if (editorMode == null) {
+			return null;
+		} else if (editorMode == Mode.CREATE) {
+			return new Object[] { editorMode };
+		} else if (editorMode == Mode.UPDATE) {
+			return new Object[] { editorMode, editorSystemParamId };
+		} else {
+			throw new IllegalStateException(editorMode.toString());
+		}
+	}
+
+	void onActionFromNewSystemParameter() {
+		editorMode = Mode.CREATE;
+		editorSystemParamId = null;
+	}
+
+	// CREATE
+
+	void onCancelNewSystemParameter() {
+		editorMode = null;
+		editorSystemParamId = null;
+	}
+
+	void onPrepareForRenderFromFrmCreateSystemParameter() throws Exception {
+		if (frmCreateSystemParameter.isValid()) {
+			editorMode = Mode.CREATE;
+			editorSystemParam = new SystemParameter();
+		}
+	}
+
+	void onPrepareForSubmitFromFrmCreateSystemParameter() throws Exception {
+		editorMode = Mode.CREATE;
+		editorSystemParam = new SystemParameter();
+	}
+
+	@CommitAfter
+	void onValidateFromFrmCreateSystemParameter() {
+		if (frmCreateSystemParameter.getHasErrors()) {
+			return;
+		}
+		try {
+			genericService.saveOrUpdate(editorSystemParam);
+		} catch (Exception e) {
+			frmCreateSystemParameter.recordError(e.getMessage());
+		}
+	}
+
+	void onSuccessFromFrmCreateSystemParameter() {
+		editorMode = null;
+		editorSystemParamId = editorSystemParam.getSystemParameterId();
+	}
+
+	// UPDATE
+
+	void onCancelUpdate(Long systemParameterId) {
+		editorMode = null;
+		editorSystemParamId = systemParameterId;
+	}
+
+	void onPrepareForRenderFromUpdateForm() {
+		if (updateForm.isValid()) {
+			editorMode = Mode.UPDATE;
+			editorSystemParam = genericService.getByPK(SystemParameter.class, editorSystemParamId);
+		}
+	}
+
+	void onPrepareForSubmitFromUpdateForm() {
+		editorMode = Mode.UPDATE;
+		editorSystemParam = genericService.getByPK(SystemParameter.class, editorSystemParamId);
+		if (editorSystemParam == null) {
+			updateForm.recordError("Param has been deleted by another process.");
+			editorSystemParam = new SystemParameter();
+		}
+	}
+
+	@CommitAfter
+	void onValidateFromUpdateForm() {
+		logger.info("ONVALIDATE");
+		if (updateForm.getHasErrors()) {
+			return;
+		}
+		try {
+			genericService.saveOrUpdate(editorSystemParam);
+		} catch (Exception e) {
+			updateForm.recordError(e.getMessage());
+		}
+	}
+
+	void onSuccessFromUpdateForm() {
+		editorMode = null;
+		editorSystemParamId = editorSystemParam.getSystemParameterId();
+	}
+
+	//////////////////////////////
 
 	public List<SystemParameter> getSystemParameters() {
 		return (List<SystemParameter>) genericService.getAll(SystemParameter.class);
 	}
 
-	void onActionFromNewSystemParameter() {
-		editSystemParameter = new SystemParameter();
-	}
-
 	void onActionFromEditParameter(SystemParameter sp) {
-		editSystemParameter = sp;
+		editorMode = Mode.UPDATE;
+		editorSystemParamId = sp.getSystemParameterId();
 	}
 
 	@CommitAfter
@@ -68,9 +209,4 @@ public class SystemParameters {
 		genericService.delete(sp);
 	}
 
-	@CommitAfter
-	void onSuccessFromEditSystemParameter() {
-		genericService.saveOrUpdate(editSystemParameter);
-		editSystemParameter = null;
-	}
 }
